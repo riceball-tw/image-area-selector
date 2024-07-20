5ad3df981d23939a0cde40168d2b19b578b1971a
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-07-21 06:00:22 +0800

    refactor: Extract getSelectionInfo as utility function

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Window from "@components/Window";
 import ImageUploader from "@components/ImageUploader";
 import Terminal from "@components/Terminal";
 import { type IArea } from "@bmunozg/react-image-area";
-import type { ImageDimension } from "@type/ImageSelector";
+import { getSelectionsInfo } from "@utility/getSelectionsInfo";
 
 export function App() {
   const [selections, setSelections] = useState<IArea[]>([]);
@@ -12,20 +12,6 @@ export function App() {
     width: 0,
     height: 0,
   });
-
-  const getSelectionsInfo = (
-    areas: IArea[],
-    imageDimension: ImageDimension,
-  ) => {
-    return areas.map((area) => {
-      return {
-        x: Math.round((area.x / 100) * imageDimension.width),
-        y: Math.round((area.y / 100) * imageDimension.height),
-        width: Math.round((area.width / 100) * imageDimension.width),
-        height: Math.round((area.height / 100) * imageDimension.height),
-      };
-    });
-  };
 
   const selectionsInfo = getSelectionsInfo(selections, imageDimension);
 
---
+++ src/utility/getSelectionsInfo.ts
@@ -0,0 +1,15 @@
+import type { ImageDimension, AreaInfo } from "@type/ImageSelector";
+
+export function getSelectionsInfo(
+  selections: AreaInfo[],
+  imageDimension: ImageDimension,
+) {
+  return selections.map((area) => {
+    return {
+      x: Math.round((area.x / 100) * imageDimension.width),
+      y: Math.round((area.y / 100) * imageDimension.height),
+      width: Math.round((area.width / 100) * imageDimension.width),
+      height: Math.round((area.height / 100) * imageDimension.height),
+    };
+  });
+}
