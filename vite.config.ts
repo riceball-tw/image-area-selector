36c8524a4981c0411e625a467b0f9f314882f3aa
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-07-20 15:34:24 +0800

    chore: Add utility path alias

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
+      "@utility": path.resolve(__dirname, "./src/utility"),
     },
   },
   plugins: [react()],
